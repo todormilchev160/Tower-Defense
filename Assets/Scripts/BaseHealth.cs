7bec31d04a29f09bec6c7a28b0d9f8ac5743258e
todormilchev160 <bavnotora@gmail.com>
2026-09-16 13:58:56 +0300

    Base

--- Assets/Scripts/BaseHealth.cs
+++ Assets/Scripts/BaseHealth.cs
@@ -2,15 +2,22 @@ using UnityEngine;
 
 public class BaseHealth : MonoBehaviour
 {
+    [SerializeField]private float baseHealth=100;
+    private EnemyDamageBase enemyDamageBase;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
-
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(baseHealth);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        enemyDamageBase=other.GetComponent<EnemyDamageBase>();
+        Debug.Log("Triggered");
+        baseHealth-=enemyDamageBase.damageDealtOnBase;
     }
 }
